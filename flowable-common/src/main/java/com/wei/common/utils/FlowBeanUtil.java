88ff5c0e89c37f7933b75bfd23be72fc00eef744
yuwei <yuwei@yuweix.com>
2023-07-23 18:36:25 +0800

    ...

--- flowable-common/src/main/java/com/wei/common/utils/FlowBeanUtil.java
+++ flowable-common/src/main/java/com/wei/common/utils/FlowBeanUtil.java
@@ -2,16 +2,8 @@ package com.wei.common.utils;
 
 
 import com.yuweix.tripod.core.BeanUtil;
-import java.util.List;
 
 
 public abstract class FlowBeanUtil extends BeanUtil {
-    public static<T> boolean exists(List<T> list, T one){
-        for (T t: list) {
-            if (t.equals(one)) {
-                return true;
-            }
-        }
-        return false;
-    }
+
 }
